24dd18d5614cff1996413fe0264dba3f8dfa94dc
ApriliaCristy <ianaleale123@gmail.com>
2024-05-31 22:59:06 +0700

    new commit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,6 @@ class DashboardController extends Controller
 {
     public function total()
     {
-
         $totalUser = User::getTotalUser();
         $totalProducts = Product::getTotalProducts();
         $totalOrder = Order::getTotalOrder();
@@ -21,21 +20,25 @@ class DashboardController extends Controller
         $totalCoffe = Product::where('category', '1')->count();
         $totalNon = Product::where('category', '0')->count();
 
+        // Inisialisasi variabel
+        $productName = null;
+        $totalPurchased = 0;
+
         $mostPurchasedProduct = Transaction::select('product_id', DB::raw('SUM(amount) as total'))
-    ->groupBy('product_id')
-    ->orderByDesc('total')
-    ->first();
+            ->groupBy('product_id')
+            ->orderByDesc('total')
+            ->first();
 
-    if ($mostPurchasedProduct) {
-    $product = Product::find($mostPurchasedProduct->product_id);
+        if ($mostPurchasedProduct) {
+            $product = Product::find($mostPurchasedProduct->product_id);
 
-    if ($product) {
-        $productName = $product->name;
-        $totalPurchased = $mostPurchasedProduct->total;
-    }
-}
+            if ($product) {
+                $productName = $product->name;
+                $totalPurchased = $mostPurchasedProduct->total;
+            }
+        }
 
-    $products = Product::all();
+        $products = Product::all();
 
         $totalSalesPerProduct = [];
 
@@ -45,34 +48,31 @@ class DashboardController extends Controller
             $totalSalesPerProduct[$product->name] = $totalSales;
         }
 
+        $products = Product::withSum('transactions', 'amount')->get();
 
-        $products = Product::withSum('transactions', 'amount')->get();;
+        // Mengumpulkan data nama produk dan total penjualan untuk digunakan di grafik
+        $namaProduk = $products->pluck('name');
+        $totalSold = $products->pluck('transactions_sum_amount');
 
-    // Mengumpulkan data nama produk dan total penjualan untuk digunakan di grafik
-    $namaProduk = $products->pluck('name');
-    $totalSold = $products->pluck('transactions_sum_amount');
+        $usersWithOrderCount = Order::select('user_id', DB::raw('COUNT(*) as order_count'))
+            ->groupBy('user_id')
+            ->get();
 
+        $userNames = [];
+        $orderCounts = [];
 
+        foreach ($usersWithOrderCount as $userOrder) {
+            $user = User::find($userOrder->user_id);
+            if ($user) {
+                $userName = $user->name;
+                $orderCount = $userOrder->order_count;
 
-    $usersWithOrderCount = Order::select('user_id', DB::raw('COUNT(*) as order_count'))
-    ->groupBy('user_id')
-    ->get();
+                $userNames[] = $userName;
+                $orderCounts[] = $orderCount;
+            }
+        }
 
-$userNames = [];
-$orderCounts = [];
-
-foreach ($usersWithOrderCount as $userOrder) {
-    $user = User::find($userOrder->user_id);
-    if ($user) {
-        $userName = $user->name;
-        $orderCount = $userOrder->order_count;
-
-        $userNames[] = $userName;
-        $orderCounts[] = $orderCount;
-    }
-}
-
-            $coffeeSold = Product::where('category', 1)
+        $coffeeSold = Product::where('category', 1)
             ->whereHas('transactions')
             ->with(['transactions' => function ($query) {
                 $query->whereHas('order');
@@ -82,7 +82,7 @@ foreach ($usersWithOrderCount as $userOrder) {
                 return $total + $product->transactions->sum('amount');
             }, 0);
 
-            $nonCoffeeSold = Product::where('category', 0)
+        $nonCoffeeSold = Product::where('category', 0)
             ->whereHas('transactions')
             ->with(['transactions' => function ($query) {
                 $query->whereHas('order');
@@ -92,16 +92,16 @@ foreach ($usersWithOrderCount as $userOrder) {
                 return $total + $product->transactions->sum('amount');
             }, 0);
 
-            $paidOrders = Order::where('is_paid', true)->get();
+        $paidOrders = Order::where('is_paid', true)->get();
 
-            // Menghitung total pendapatan dari semua pengguna
-            $totalEarning = $nonCoffeeSold + $coffeeSold;
+        // Menghitung total pendapatan dari semua pengguna
+        $totalEarning = $nonCoffeeSold + $coffeeSold;
 
-
-
-
-
-        return view('dashboard_admin', compact('totalUser', 'totalProducts', 'totalOrder', 'totalCoffe','totalNon', 'productName',
-        'totalPurchased', 'totalSalesPerProduct', 'totalSales', 'productName', 'namaProduk', 'totalSold', 'userNames', 'orderCounts','coffeeSold','nonCoffeeSold', 'totalEarning'));
+        return view('dashboard_admin', compact(
+            'totalUser', 'totalProducts', 'totalOrder', 'totalCoffe', 'totalNon',
+            'productName', 'totalPurchased', 'totalSalesPerProduct', 'totalSales',
+            'namaProduk', 'totalSold', 'userNames', 'orderCounts', 'coffeeSold',
+            'nonCoffeeSold', 'totalEarning'
+        ));
     }
 }
